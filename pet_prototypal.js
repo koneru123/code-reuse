c898c3d1220e9bbadbf873ebd5161e225aae00d0
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-19 16:55:15 -0800

    clean-up

--- pet_prototypal.js
+++ pet_prototypal.js
@@ -30,21 +30,3 @@
 
  var dog = Pet('snuufy', 'germansheperd', 3);
  var cat = Pet('kitty', 'meow', 4);
-
- /*Advanced Content
- Add the following methods
- play -> accept unit of time and increase happiness by number of units of time until zero energy
- Age the pet once during play
- nap -> accept unit of time and increase energy by number of units of time until 100 energy
- Age the pet for every cycle of 100 units of time used by playing or napping.
- Create a traits property as an array of objects (initially empty)
-
-  traits: [
-            { eyeColor: "purple"},
-            { hairLength: "long" },
-            ... etc
-          ]
- Create a method to add traits to a pet.
-
- Create the ability for your pet to create a child
- Have the pet inherit at least 1 trait from the parent.*/
